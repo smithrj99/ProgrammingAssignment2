a36911b824ccb260dc5b3f371d6f9d2ac802bde0
smithrj99 <smithrj_99@hotmail.com>
2014-09-21 14:48:55 -0700

    Implement the functions for the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+##########################################################################
+## Programming Assignment 2 is to implement functions that will
+## maintain a cache, and leverage the cache when called again
+## with the same input.
+##
+## In the example, it keeps track of a vector and its mean. 
+## In this implementation, it keeps track of a matrix and its inversion.
+##
+## makeCacheMatrix:
+## This function will create the cache for matrix inversion, including
+## methods to get/set the matrix and its inversion.
+##
+## cacheSolve:
+## This function will compute the inversion, either from the cache if
+## the inversion has already be determined for a given matrix, or using
+## the solve function, and then storing the inverted matrix to the cache
+## for subsequent retrieval
+##########################################################################
 
-## Write a short comment describing this function
+
+## This function will create the cache for matrix inversion, including
+## methods to get/set the matrix and its inversion.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  cachedInversion <- NULL
 
+## Function to set the matrix.  
+  set <- function(inputMatrix) {
+    x <<- inputMatrix
+    cachedInversion <<- NULL
+  }
+
+## Function to get the matrix.  
+  get <- function() x
+
+## Function to set the matrix's inversion
+  setInversion <- function(inversion) cachedInversion <<- inversion
+
+## Function to get the matrix's inversion
+  getInversion <- function() cachedInversion
+
+
+## Return list of the above functions
+  list(set = set, get = get,
+       setInversion = setInversion,
+       getInversion = getInversion)
 }
 
-
-## Write a short comment describing this function
+## This function will return the inversion of the cache passed in.
+## It will either return the stored inversion of the matrix,
+## or calculate the inversion, store it in cache for later use,
+## and then return the inversion.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## Get the inversion, and return it exists  
+  cachedInversion <- x$getInversion()
+  if (!is.null(cachedInversion)) {
+    message("getting cached inversion")
+    return(cachedInversion)
+  }
+
+## Only get here if the inversion didn't exist. Get the matrix, get the
+## inversion, store it, then return it
+  myMatrix <- x$get()
+  inversion <- solve(myMatrix, ...)
+  x$setInversion(inversion)
+  inversion
 }
